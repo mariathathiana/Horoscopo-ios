dc28e4aa6e3116ed23622cfbe71169bcde004fb5
mariathathiana <109003131+mariathathiana@users.noreply.github.com>
2025-11-06 13:55:32 +0100

    Dos label incorporados

--- Horoscopo-ios/Horoscope.swift
+++ Horoscopo-ios/Horoscope.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import UIKit
+
+struct Horoscope {
+    let id: String
+    let name: String
+    let dates: String
+ 
+    func getSignIcon() -> UIImage {
+        return UIImage(named: "horoscope_icons/(id)_icon")!
+    }
+    
+    static let horoscopeList = [
+        Horoscope(id: "aries", name: "Aries", dates: "March 21 to Abril 19")
+    ]
+    
+    static func getAll() -> [Horoscope] {
+
+        return horoscopeList
+    }
+    
+    static func getById( id: String) -> Horoscope {
+        return Horoscope.getAll().first(where: { $0.id == id})!
+        
+    }
+    
+}
